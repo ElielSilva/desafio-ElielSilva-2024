90f120a9847e9f51e39655df0d51a867228b9933
ElielSilva <silvaeliel940@gmail.com>
2024-09-11 17:33:20 -0300

    [FEAT] - created validation for params animal and quantidade

---
+++ src/especies-zoo.js
@@ -0,0 +1,11 @@
+    
+const data = {
+    "LEAO": { tamanho: 3, bioma: ['savana'], biomas: [1,3,5], carnivoros: true },
+    'LEOPARDO': { tamanho:	2, bioma: ['savana'], biomas: [1,3,5], carnivoros: true },
+    'CROCODILO': { tamanho:	3, bioma: ['rio'], biomas: [3,4], carnivoros: true },
+    'MACACO':   { tamanho: 1, bioma: ['savana', 'floresta'], biomas: [1,2,3,5], carnivoros: false },
+    'GAZELA':   { tamanho: 2 , bioma: ['savana'], biomas: [1,3,5], carnivoros: false },
+    'HIPOPOTAMO': { tamanho: 4 , bioma:['savana, rio'], biomas: [1,3,4,5], carnivoros: false },
+ };
+
+ export { data as especies };
--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,8 +1,15 @@
+import { especies } from "./especies-zoo";
+
 class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
+        if(!especies[animal]) {
+            return {erro: "Animal inválido"}
+        }
+        if(quantidade <= 0) {
+            return {erro: "Quantidade inválida"}
+        }
     }
-
 }
 
 export { RecintosZoo as RecintosZoo };
