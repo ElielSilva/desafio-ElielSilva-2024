1775dbe240b40ab56877b065b8e76988277d08c0
ElielSilva <silvaeliel940@gmail.com>
2024-09-11 19:08:18 -0300

    [FEAT] - create the validarExistenciaDeVagas function to check if there is space for the new animals

---
+++ src/recintos-base-zoo.js
@@ -0,0 +1,11 @@
+const base = {
+    numero: {
+    1: { bioma: 'savana',tamanhoTotal: 10, animais: ['MACACO', 'MACACO' , "MACACO"]},
+    2: { bioma: 'floresta',tamanhoTotal: 5, animais: []},
+    3: { bioma: 'savana e rio', tamanhoTotal: 7, animais: ['GAZELA'] },
+    4: { bioma: 'rio', tamanhoTotal: 8, animais: [] },
+    5: { bioma: 'savana', tamanhoTotal: 9, animais: ['LEAO'] }
+    }
+};
+
+export { base as recintosBase };
--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,13 +1,66 @@
 import { especies } from "./especies-zoo";
+import { recintosBase } from "./recintos-base-zoo";
 
 class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
-        if(!especies[animal]) {
-            return {erro: "Animal inválido"}
+        const resulatadoValidacao = this.validarParametros(animal,quantidade);
+        if (resulatadoValidacao.resultadoErro) {
+            return resulatadoValidacao.dados;
         }
+
+        const resultado = this.validarExistenciaDeVagas(animal,quantidade);
+        
+        if (resultado.resultadoErro) {
+            return { erro: "Não há recinto viável"}
+        }
+
+    }
+     
+    validarParametros(animal, quantidade) {
+        if (!especies[animal]) {
+            return {
+            resultadoErro: true,
+            dados:{
+                    erro: "Animal inválido"
+            }
+            }
+        };
+        
         if(quantidade <= 0) {
-            return {erro: "Quantidade inválida"}
+            return {
+            resultadoErro: true,
+            dados:{
+                erro: "Quantidade inválida"
+            }
+            }
+        };
+    
+        return {resultadoErro: false}
+    }
+
+    validarExistenciaDeVagas(animal,quantidade) {
+        const resultado = especies[animal].biomas.filter((bioma) => {
+            const quantidadeOcupadaEspecieEntrada = especies[animal].tamanho * quantidade
+            const result = recintosBase.numero[bioma].animais.reduce((arr, curr) => {
+                 if (especies[animal].carnivoros != especies[curr].carnivoros) {
+                     arr.especiesDiferentes = true
+                 }
+                 arr.total = arr.total + especies[curr].tamanho;
+                 return arr
+            }, {especiesDiferentes: false , total: 0})
+
+            if (quantidadeOcupadaEspecieEntrada > (recintosBase.numero[bioma].tamanhoTotal - result.total)) {
+                 return false
+            }
+            if (result.especiesDiferentes) {
+                return false
+            }
+            return true
+        });
+        return {
+            resultadoErro: !resultado.length,
+            dados: resultado
         }
     }
 }
